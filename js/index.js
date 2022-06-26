74d83f00bdba03ff13dbd87cfc01c404e6d6a9c1
Frank_hsu <gary80519@yahoo.com.tw>
2022-06-26 16:28:12 +0800

    改連結至yt data尚未改寫

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 $(function () {
-    $('.card').click(function(){
-        console.log($(this).find('img').attr('src'));
+    // $('.card').click(function(){
+    //     console.log($(this).find('img').attr('src'));
 
-        call_video()
-        $('#staticBackdrop').modal('show')
-    })
+    //     call_video()
+    //     $('#staticBackdrop').modal('show')
+    // })
     
-    $('.btn-close').click(function(){
-        $('.modal-body').html('')
-    })
+    // $('.btn-close').click(function(){
+    //     $('.modal-body').html('')
+    // })
 })
 
 
-function call_video(){
-    var str ='<iframe width="100%" height="315" src="https://www.youtube.com/embed/LSNoRzojcQo?autoplay=1" allow="autoplay;"></iframe>'
-    $('.modal-body').html(str)
-}
+// function call_video(){
+//     var str ='<iframe width="100%" height="315" src="https://www.youtube.com/embed/1vcy7pncG-Y?autoplay=1" allow="autoplay;"></iframe>'
+//     $('.modal-body').html(str)
+// }
